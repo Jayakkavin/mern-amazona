ea3d7b715eaa1069b3f700b698d161fb0e8abd27
Jayakkavin <pejayakkavin@gmail.com>
2022-06-27 15:17:58 +0530

    Backend

--- README.txt
+++ README.txt
@@ -1,5 +1,13 @@
+cd frontend
+rm -rf .git
 $ git config --global user.name "Jayakkavin"
 $ git config --global user.email pejayakkavin@gmail.com
 
 React:
     npx create-react-app frontend
+    npm i react-router-dom
+
+Node :
+    npm init
+    npm install express
+    npm install nodemon --save-dev
---
+++ backend/data.js
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpeg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality shirt',
+    },
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpeg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality shirt',
+    },
+  ],
+};
+
+export default data;
